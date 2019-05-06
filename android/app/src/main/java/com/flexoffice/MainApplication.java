eb0c7887ba67d15b628ba2484d954d1212385a67
Nikola Tomic <ntomic@3d4pro.fr>
2019-05-06 11:08:41 +0200

    test build

--- android/app/src/main/java/com/flexoffice/MainApplication.java
+++ android/app/src/main/java/com/flexoffice/MainApplication.java
@@ -18,6 +18,7 @@ package com.flexoffice;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.tradle.react.UdpSocketsModule;
 import com.peel.react.TcpSocketsModule;
 import com.bitgo.randombytes.RandomBytesPackage;
@@ -53,6 +54,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNDeviceInfo(),
             new UdpSocketsModule(),
             new TcpSocketsModule(),
             new RandomBytesPackage(),
